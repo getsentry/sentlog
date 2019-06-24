5029f614afc2271306448ac70908538e8ef08b98
Anton Ovchinnikov <anton@tonyo.info>
2019-06-24 14:03:43 +0200

    feat: Send an example event

---
+++ go.mod
@@ -0,0 +1,5 @@
+module log-sdk
+
+go 1.12
+
+require github.com/getsentry/sentry-go v0.1.0
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/getsentry/sentry-go"
+)
 
 func main() {
-    fmt.Println("hello world")
+	fmt.Println("init")
+
+	err := sentry.Init(sentry.ClientOptions{})
+
+	if err != nil {
+		fmt.Printf("Sentry initialization failed: %v\n", err)
+	}
+
+	_, err = os.Open("INVALID")
+	if err != nil {
+		fmt.Println("Capturing...")
+		eventID := sentry.CaptureException(err)
+		fmt.Println(*eventID)
+	}
+
+	sentry.Flush(time.Second * 5)
 }
